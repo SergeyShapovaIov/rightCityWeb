73aae2173edface900ef42be87f87660d3cf9602
sergey shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-07-06 17:55:05 +0300

    create new branch

--- src/main/java/com/example/rightCityWeb/RightCityWebApplication.java
+++ src/main/java/com/example/rightCityWeb/RightCityWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RightCityWebApplication {
-
+	// create new branch, potom stelad delete
 	public static void main(String[] args) {
 		SpringApplication.run(RightCityWebApplication.class, args);
 	}
