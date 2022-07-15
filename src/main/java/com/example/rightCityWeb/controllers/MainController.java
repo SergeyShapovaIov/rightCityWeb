aecba941e14f87eb45272f5fc62118925a75ab7e
sergey shapovalov <Shapovalov.Sergey2016@yandex.ru>
2022-07-15 22:44:24 +0300

    main page refactor

--- src/main/java/com/example/rightCityWeb/controllers/MainController.java
+++ src/main/java/com/example/rightCityWeb/controllers/MainController.java
@@ -2,11 +2,20 @@ package com.example.rightCityWeb.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String mainPage() {
+    @GetMapping("/post")
+    public String postPage() {
         return "post";
+    }
+
+    @GetMapping("/main")
+    public String mainPage() {
+        ModelAndView model = new ModelAndView("main");
+        /// model.addAttribute("variableName", "someValue");
+
+        return "main";
     }
 }
